a5e146ef41f8381bc2bebb01ca6299c3fcfdfd46
Duncan Eddy <duncan.eddy@gmail.com>
2025-01-18 12:22:59 -0800

    Update first time install

--- setup_new_machine.sh
+++ setup_new_machine.sh
@@ -41,6 +41,9 @@ cp ./git/gitconfig ~/.gitconfig
 cp ./git/gitmessage ~/.gitmessage
 cp ./git/gitignore_global ~/.gitignore_global
 
-# Install tex
-
-# Install Docker
+# Copy config files
+cp aliases.sh ~/.aliases.sh
+cp zshenv ~/.zshenv
+cp zshrc ~/.zshrc
+cp aliases ~/.aliases.sh
+cp vimrc ~/.vimrc
